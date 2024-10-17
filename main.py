2e389b514be527fd948b279179ddb8104f68d4bd
Sk3l3wu <stlegend38@gmail.com>
2024-10-17 11:11:59 +0300

    version 1 done by garang

---
+++ main.py
@@ -0,0 +1,16 @@
+a = int(input("Enter the First number: "))
+b = int(input("Enter the Second number: "))
+
+def addition(a, b):
+    return a + b 
+#def subtraction(a, b):
+    return a - b
+#def multiplion(a, b):
+    return a * b
+#def division(a, b):
+    return a / b
+
+print(addition(a, b))
+#print(subtraction(a, b))
+#print(multiplion(a, b))
+#print(division(a, b))
